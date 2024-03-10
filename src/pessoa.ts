c4e03c3ee257831fa202b19b76aaac9ae6760c2a
famgz <famgz@proton.me>
2024-03-09 23:04:19 -0300

    class Pessoa

---
+++ src/pessoa.ts
@@ -0,0 +1,56 @@
+export class Pessoa {
+  _nome: string;
+  _idade: number;
+  _email: string;
+
+  constructor(nome: string, idade: number, email: string) {
+    if (this.nomeInvalido(nome)) {
+      throw new Error('Nome inválido');
+    }
+    this._nome = nome;
+    this._idade = idade;
+    this._email = email;
+  }
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  get idade(): number {
+    return this._idade;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set nome(novoNome: string) {
+    this._nome = novoNome;
+  }
+
+  set idade(novaIdade: number) {
+    this._idade = novaIdade;
+  }
+
+  set email(novoEmail: string) {
+    this._email = novoEmail;
+  }
+
+  private ehNumero(letra: string): boolean {
+    return !isNaN(parseInt(letra));
+  }
+
+  private nomeInvalido(nome: string) {
+    if (nome.length < 4) {
+      console.error('Erro: Nome deve ao menos 4 caracteres');
+      return true;
+    }
+    for (const letra of nome) {
+      if (this.ehNumero(letra)) {
+        console.error('Erro: Nome não pode conter caracteres numéricos');
+        return true;
+      }
+    }
+    return false;
+  }
+}
