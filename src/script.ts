e798fa27dcb4d0c1fbf07b58f3e7c38c860220f4
famgz <famgz@proton.me>
2024-03-11 13:41:35 -0300

    configuracao de testes isolada

--- src/script.ts
+++ src/script.ts
@@ -1,25 +1,8 @@
 import { BancoDeDados } from './bancoDeDados';
-import { Pessoa } from './pessoa';
 import { Menu } from './menu';
-import { runSeed } from './seed';
 
 const menu = new Menu();
-// menu.exibirMenu()
 
 const bancoDados = new BancoDeDados();
 
-runSeed(bancoDados);
-
-bancoDados.listar();
-
-console.log(bancoDados.buscarPeloNome('Lucas'));
-
-// menu.exibirMenu();
-
-bancoDados.adicionar(new Pessoa('teste', 24, 'testando'));
-
-bancoDados.listar();
-
-bancoDados.adicionar(new Pessoa('teste alterado', 32, 'testando alterado'));
-
-bancoDados.listar();
+menu.exibirMenu();
