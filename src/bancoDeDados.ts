e660fa54e380be9ea9d722d09bd8d8398c0a350c
famgz <famgz@proton.me>
2024-03-11 12:47:11 -0300

    refatoração método buscarPeloNome como privado. Implementação inicial dos demais métodos: listar, atualizar, deletar

--- src/bancoDeDados.ts
+++ src/bancoDeDados.ts
@@ -7,10 +7,14 @@ export class BancoDeDados {
     this._listaDePessoas = [];
   }
 
-  adicionar(pessoa: Pessoa): boolean {
-    const pessoaExiste = this._listaDePessoas.find(
-      (p) => p.nome.toLowerCase() === pessoa.nome.toLowerCase()
+  private _buscarPeloNome(nome: string): Pessoa | undefined {
+    return this._listaDePessoas.find(
+      (alvo) => alvo.nome.toLowerCase() === nome.toLowerCase()
     );
+  }
+
+  adicionar(pessoa: Pessoa): boolean {
+    const pessoaExiste = this._buscarPeloNome(pessoa.nome);
     if (pessoaExiste) {
       console.error(`${pessoa} já existe`);
       return false;
@@ -20,31 +24,45 @@ export class BancoDeDados {
     return true;
   }
 
-  listar(){
-    console.log(
-    `Pessoas cadastradas:
-    ${this._listaDePessoas}`);
+  listar(): void {
+    console.log('PESSOAS CADASTRADAS');
+    const printObj = this._listaDePessoas.map((p) => ({
+      nome: p.nome,
+      idade: p.idade,
+      email: p.email,
+    }));
+    console.table(printObj);
   }
 
   buscarPeloNome(nome: string): Pessoa | undefined {
-    const pessoa = this._listaDePessoas.find(
-      (alvo) => alvo.nome.toLowerCase() === nome.toLowerCase()
-    );
+    const pessoa = this._buscarPeloNome(nome);
     if (!pessoa) {
-      throw Error(`\nPessoa ${nome} não encontrada`);
+      console.error(`\nPessoa "${nome}" não encontrada`);
+    } else {
+      console.log(
+        `\nEncontrado pessoa com nome "${nome}". Imprimindo dados do cadastro..:`
+      );
     }
-    console.log(`\nEncontrado pessoa com nome ${nome}. Imprimindo dados do cadastro..:`);
-    
     return pessoa;
   }
 
   atualizar(pessoa: Pessoa): boolean {
+    const index = this._listaDePessoas.indexOf(pessoa);
+    if (index === -1) {
+      console.error(`\nPessoa "${pessoa.nome}" não encontrada`);
+      return false;
+    }
+    this._listaDePessoas[index] = pessoa;
     return true;
   }
 
   deletar(pessoa: Pessoa): boolean {
+    const index = this._listaDePessoas.indexOf(pessoa);
+    if (index === -1) {
+      console.error(`\nPessoa "${pessoa.nome}" não encontrada`);
+      return false;
+    }
+    delete this._listaDePessoas[index];
     return true;
   }
-
-
 }
