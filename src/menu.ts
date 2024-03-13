298a65948a858d997b42aaf8921a92d1adf57d26
famgz <famgz@proton.me>
2024-03-13 14:15:17 -0300

    refatoracao classe ListaMenu dentro do arquivo menu.ts por afinidade

--- src/menu.ts
+++ src/menu.ts
@@ -1,120 +1,117 @@
 import promptSync from 'prompt-sync';
 import { BancoDeDados } from './bancoDeDados';
-import { Pessoa} from "./pessoa"
-import { ListaMenu} from "./listaMenu"
+import { Pessoa } from './pessoa';
 
+abstract class ListaMenu {
+  _lista: string;
 
-
-
-export class Menu extends ListaMenu{
-
-  private _bancoDeDados : BancoDeDados
-  
-  constructor(bancoDeDados: BancoDeDados){
-    super();
-    this._bancoDeDados = bancoDeDados
-    
-
+  constructor() {
+    this._lista = `
+    1 - Adicionar 
+    2 - Listar
+    3 - Buscar pelo nome 
+    4 - Atualizar 
+    5 - Deletar 
+    `;
   }
 
-  
+  mostrarMenu(): void {
+    console.log(this._lista);
+  }
 
-  exibirMenu () : void{
-    while (true){
-      let escolha :null| number = null
-      const prompt = promptSync()
+  abstract exibirMenu(): void;
+}
 
-      do{
-        console.clear()
-        this.mostrarMenu()
+export class Menu extends ListaMenu {
+  private _bancoDeDados: BancoDeDados;
+
+  constructor(bancoDeDados: BancoDeDados) {
+    super();
+    this._bancoDeDados = bancoDeDados;
+  }
+
+  exibirMenu(): void {
+    while (true) {
+      let escolha: null | number = null;
+      const prompt = promptSync();
+
+      do {
+        console.clear();
+        this.mostrarMenu();
         console.log();
-        
-        escolha = Number(prompt("Escolhe uma opção. Digite 0 caso queira sair: ".trim()))
-      
-        
-        if (escolha > 5 || escolha < 1){
-          continue
-        }
-        if (isNaN(escolha)){
-          continue
-        }
 
-      }while(escolha === null)
+        escolha = Number(
+          prompt('Escolhe uma opção. Digite 0 caso queira sair: '.trim())
+        );
 
-      if (escolha === 0){
-        return
+        if (escolha > 5 || escolha < 1) {
+          continue;
+        }
+        if (isNaN(escolha)) {
+          continue;
+        }
+      } while (escolha === null);
+
+      if (escolha === 0) {
+        return;
       }
 
-      switch(escolha){
-
+      switch (escolha) {
         case 1:
+          const nome = prompt('Informe o nome: ');
+          const idade = Number(prompt('Informe a idade: '));
+          const email = prompt('Informe o email: ');
+          const pessoa = new Pessoa(nome, idade, email);
+          this._bancoDeDados.adicionar(pessoa);
 
-          const nome = prompt("Informe o nome: ")
-          const idade =Number(prompt("Informe a idade: "))
-          const email =prompt("Informe o email: ")
-          const pessoa = new Pessoa(nome,idade,email)
-          this._bancoDeDados.adicionar(pessoa)
-
-        break
+          break;
 
         case 2:
           this._bancoDeDados.listar();
 
-          break
+          break;
 
         case 3:
-          const buscarNome : string = prompt("Informe o nome.: ")
+          const buscarNome: string = prompt('Informe o nome.: ');
           this._bancoDeDados.buscarPeloNome(buscarNome);
-          break
+          break;
 
         case 4:
-          
           const pessoaAntiga = this._bancoDeDados.buscarPorId(this.pedirId());
-          const pessoaNova = this.pedirDadosAtualizar()
-          this._bancoDeDados.atualizar(pessoaAntiga,pessoaNova)
+          const pessoaNova = this.pedirDadosAtualizar();
+          this._bancoDeDados.atualizar(pessoaAntiga, pessoaNova);
 
-          break
+          break;
 
         case 5:
-          
-          const pessoaDeletar = this._bancoDeDados.buscarPorId(this.pedirId())
+          const pessoaDeletar = this._bancoDeDados.buscarPorId(this.pedirId());
           this._bancoDeDados.deletar(pessoaDeletar);
-        
-          break
-      
+
+          break;
+
         default:
-          throw Error("Erro interno. problema em chamar os métodos, reveja o switch")
-        
+          throw Error(
+            'Erro interno. problema em chamar os métodos, reveja o switch'
+          );
       }
       prompt('Aperte ENTER para voltar ao menu');
     }
   }
 
-  private pedirId(): number{
-    const prompt = promptSync()
+  private pedirId(): number {
+    const prompt = promptSync();
     console.log(`Usuário cadastrados:\n`);
-    this._bancoDeDados.listar()
-    const entrada = Number(prompt("Informe o Id: "))
-    return entrada
+    this._bancoDeDados.listar();
+    const entrada = Number(prompt('Informe o Id: '));
+    return entrada;
+  }
+
+  private pedirDadosAtualizar() {
+    const prompt = promptSync();
+    const novoNome: string = String(prompt('Informe o novo nome: '));
+    const novaIdade: number = Number(prompt('Informe a nova idade: '));
+    const novoEmail: string = String(prompt('Informe o novo email: '));
+    const pessoaAtualizada: Pessoa = new Pessoa(novoNome, novaIdade, novoEmail);
+    return pessoaAtualizada;
+  }
 }
-
-
-private pedirDadosAtualizar(){
-  const prompt = promptSync()
-  const novoNome : string = String(prompt("Informe o novo nome: "))
-  const novaIdade : number = Number(prompt("Informe a nova idade: "))
-  const novoEmail : string  = String(prompt("Informe o novo email: "))
-  const pessoaAtualizada : Pessoa = new Pessoa(novoNome,novaIdade,novoEmail)
-
-return pessoaAtualizada
-  
-
-
-
-
-}
-
-
-}
-
-
